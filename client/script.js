9991271124d90136c2fdad541a9f02798c92e44d
Entita <entitak@gmai.com>
2021-08-06 23:05:39 +0200

    adding svg's

--- client/script.js
+++ client/script.js
@@ -2,15 +2,16 @@ var app = new Vue({
     el: '#game',
     data() {
         return {
-            
+
         }
     },
     created() {
-        console.log(window.innerWidth, window.innerHeight)
+        const cursor = document.querySelector('.cursor')
+
+        document.addEventListener('mousemove', e => {
+            cursor.setAttribute("style", "top:" + (e.pageY - 10) + "px; left:" + (e.pageX - 10) + "px")
+        })
     },
     methods: {
-        test() {
-            
-        }
     }
 })
