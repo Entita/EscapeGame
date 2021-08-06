5b9911daab0959623b8ebd559397752b00633f43
Entita <entitak@gmai.com>
2021-08-06 19:28:51 +0200

    test

--- server/server.js
+++ server/server.js
@@ -4,11 +4,7 @@ const app = express();
 const server = require('http').createServer(app);
 
 // Database config
-var books = []
-
-fetch('database/books.json')
-    .then(response => response.json())
-    .then(json => books = json)
+var books = require('/database/books.json');
 
 server.listen(process.env.PORT || 3000, () => {
     console.log('Server is listening ...', books);
