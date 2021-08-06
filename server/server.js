9df7c2360872ccccfcd8469cf606fc46e5683183
Entita <entitak@gmai.com>
2021-08-06 19:45:02 +0200

    test

--- server/server.js
+++ server/server.js
@@ -6,34 +6,34 @@ const app = express();
 const server = require('http').createServer(app);
 
 // Reading file
-// fs.readFile('./database/books.json', 'utf-8', (err, jsonString) => {
-//     if (err) {
-//         console.log(err)
-//     } else {
-//         try {
-//             const data = JSON.parse(jsonString)
-//             console.log(data.name)
-//         } catch (err) {
-//             console.log('Error parsing JSON', err)
-//         }
-//     }
-// })
-
-// Writing file
-const data = {
-    name: 'Newbie Corp',
-    order_count: 0,
-    adress: 'Po Box City'
-},
-    jsonString = JSON.stringify(data)
-
-fs.writeFile('./database/test.json', jsonString, err => {
+fs.readFile('./database/test.json', 'utf-8', (err, jsonString) => {
     if (err) {
         console.log(err)
     } else {
-        console.log('File succesfully created!')
+        try {
+            const data = JSON.parse(jsonString)
+            console.log(data.name)
+        } catch (err) {
+            console.log('Error parsing JSON', err)
+        }
     }
 })
+
+// Writing file
+// const data = {
+//     name: 'Newbie Corp',
+//     order_count: 0,
+//     adress: 'Po Box City'
+// },
+//     jsonString = JSON.stringify(data, null, 2)
+
+// fs.writeFile('./database/test.json', jsonString, err => {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         console.log('File succesfully created!')
+//     }
+// })
 
 
 server.listen(process.env.PORT || 3000, () => {
