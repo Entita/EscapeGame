13e47bb424ec1005b1f56ce16354b95f973d92d3
Entita <entitak@gmai.com>
2021-08-06 19:36:16 +0200

    test

--- server/server.js
+++ server/server.js
@@ -6,9 +6,18 @@ const server = require('http').createServer(app);
 
 // Database config
 fs.readFile('/database/books.json', 'utf-8', (err, jsonString) => {
-    console.log(jsonString)
+    if (err) {
+        console.log(err)
+    } else {
+        try {
+            const data = JSON.parse(jsonString)
+            console.log(data)
+        } catch (err) {
+            console.log('Error parsing JSON', err)
+        }
+    }
 })
 
 server.listen(process.env.PORT || 3000, () => {
-    console.log('Server is listening ...', books);
+    console.log('Server is listening ...');
 });
