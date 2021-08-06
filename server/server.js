b503a3b704758e1727a277af0f0823c9b56be6e0
Entita <entitak@gmai.com>
2021-08-06 19:46:11 +0200

    test

--- server/server.js
+++ server/server.js
@@ -1,39 +1,7 @@
 // Server config
-const { json } = require('express');
 const express = require('express');
-const fs = require('fs');
 const app = express();
 const server = require('http').createServer(app);
-
-// Reading file
-fs.readFile('./database/test.json', 'utf-8', (err, jsonString) => {
-    if (err) {
-        console.log(err)
-    } else {
-        try {
-            const data = JSON.parse(jsonString)
-            console.log(data.name)
-        } catch (err) {
-            console.log('Error parsing JSON', err)
-        }
-    }
-})
-
-// Writing file
-// const data = {
-//     name: 'Newbie Corp',
-//     order_count: 0,
-//     adress: 'Po Box City'
-// },
-//     jsonString = JSON.stringify(data, null, 2)
-
-// fs.writeFile('./database/test.json', jsonString, err => {
-//     if (err) {
-//         console.log(err)
-//     } else {
-//         console.log('File succesfully created!')
-//     }
-// })
 
 
 server.listen(process.env.PORT || 3000, () => {
