42ab0de578fa480a2770bd001a32b7f3fe42c940
Entita <entitak@gmai.com>
2021-08-06 20:48:13 +0200

    test

---
+++ client/script.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+    el: '#game',
+    data() {
+        return {
+            
+        }
+    },
+    created() {
+        console.log(window.innerWidth, window.innerHeight)
+    },
+    methods: {
+        test() {
+            
+        }
+    }
+})
