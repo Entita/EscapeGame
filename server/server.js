ac1506d6124396e22ec84a1506747192196055de
Entita <entitak@gmai.com>
2021-08-06 19:43:08 +0200

    test

--- server/server.js
+++ server/server.js
@@ -1,22 +1,40 @@
 // Server config
+const { json } = require('express');
 const express = require('express');
 const fs = require('fs');
 const app = express();
 const server = require('http').createServer(app);
 
-// Database config
-fs.readFile('./database/books.json', 'utf-8', (err, jsonString) => {
+// Reading file
+// fs.readFile('./database/books.json', 'utf-8', (err, jsonString) => {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         try {
+//             const data = JSON.parse(jsonString)
+//             console.log(data.name)
+//         } catch (err) {
+//             console.log('Error parsing JSON', err)
+//         }
+//     }
+// })
+
+// Writing file
+const data = {
+    name: 'Newbie Corp',
+    order_count: 0,
+    adress: 'Po Box City'
+},
+    jsonString = JSON.stringify(data)
+
+fs.writeFile('./database/test.json', jsonString, err => {
     if (err) {
         console.log(err)
     } else {
-        try {
-            const data = JSON.parse(jsonString)
-            console.log(data)
-        } catch (err) {
-            console.log('Error parsing JSON', err)
-        }
+        console.log('File succesfully created!')
     }
 })
+
 
 server.listen(process.env.PORT || 3000, () => {
     console.log('Server is listening ...');
