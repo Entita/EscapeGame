02c225b805485e5dfd7f6a82930a35373ad2442b
Entita <entitak@gmai.com>
2021-08-10 17:16:38 +0200

    fix

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require("socket.io")(server);
 
+
 io.on('connection', socket => {
     console.log('player connected', socket.id)
     io.emit('connected', socket.id)
