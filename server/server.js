c44afc059339f18f5e947a4f8abe4224ca3370e4
Entita <entitak@gmai.com>
2021-08-12 22:01:30 +0200

    test

--- server/server.js
+++ server/server.js
@@ -5,6 +5,41 @@ const server = require('http').createServer(app);
 const io = require("socket.io")(server);
 
 
+
+
+games = {}
+function is_expired(uid) {
+    return games[uid]
+}
+function set_expire(uid, time) {
+    games[uid] = true
+    setTimeout(function () {
+        delete games[uid]
+    }, time)
+}
+
+app.get('/myroute/:id', function (req, res) {
+    /*  This route will get any url that start with /myroute/
+        For example /myroute/abc
+         /myroute/def
+    */
+    // req.params.id  ==  what the URL is entered
+    console.log(req.params.id)
+    if (is_expired(req.params.id)) return res.end('Sorry your link has expired')
+    res.send('Great you logged in!')
+})
+app.post('/login/:uniqueid', function (req, res) {
+    set_expire(req.params.uniquieid, 30 * 1000 * 60) //30 minutes = 30*60*1000 miliseconds.
+})
+
+
+
+
+
+
+
+
+
 io.on('connection', socket => {
     console.log('player connected', socket.id)
     io.emit('connected', socket.id)
