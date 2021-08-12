4fa473372f96a33fa844d4081ffdab51352d100e
Entita <entitak@gmai.com>
2021-08-13 00:35:48 +0200

    test

--- server/server.js
+++ server/server.js
@@ -5,12 +5,12 @@ const server = require('http').createServer(app);
 const io = require("socket.io")(server);
 
 // Redis
-const client = redis.createClient(6379)
+
 
 app.get('/game/:id', function (req, res) {
     const client_key = req.params.id,
         mail = 'entitak@gmail.com'
-    client.setex(mail, 3600, client_key)
+    
     res.send('Great you logged in!', mail, client_key)
 })
 
