6835db7da9faf02f48da103a6efd34c1dbaa0349
Entita <entitak@gmai.com>
2021-08-13 00:31:30 +0200

    test

--- server/server.js
+++ server/server.js
@@ -4,24 +4,15 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require("socket.io")(server);
 
-
-
+// Redis
+const client = redis.createClient(6379)
 
 app.get('/game/:id', function (req, res) {
-    const client_key = req.params.id
-    console.log(req)
-    console.log('BLA')
-    console.log(res)
-    res.send('Great you logged in!')
+    const client_key = req.params.id,
+        mail = 'entitak@gmail.com'
+    client.setex(mail, 3600, client_key)
+    res.send('Great you logged in!', mail, client_key)
 })
-
-
-
-
-
-
-
-
 
 io.on('connection', socket => {
     console.log('player connected', socket.id)
