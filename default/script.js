cefaffb869d7a195cb225ec45e164aba583fbcae
Entita <entitak@gmai.com>
2021-08-16 21:10:45 +0200

    test

--- default/script.js
+++ default/script.js
@@ -37,8 +37,26 @@ var app = new Vue({
             const cursor = document.querySelector('.cursor')
             cursor.classList.remove('expand')
         },
-        redirectURL() {
-            location.href = '/buy'
+        buyCheck() {
+            fetch('/creating-checkout-session', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    items: [
+                        { id: 1, quantity: 1 }
+                    ]
+                })
+            }).then(res => {
+                if (res.ok) return res.json()
+                return res.json().then(json => Promise.reject(json))
+            }).then(({ url }) => {
+                console.log(url)
+                // window.location = url
+            }).catch(e => {
+                console.error(e.error)
+            })
         }
     }
 })
