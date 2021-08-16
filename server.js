2c7cb6c4bc0525d37c4604550ebc8d7240e0a398
Entita <entitak@gmai.com>
2021-08-16 21:22:15 +0200

    test

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ const storeItems = new Map([
 ])
 
 app.post('/creating-checkout-session', async (req, res) => {
-    console.log('trying', req.bodyParser.body)
+    console.log('trying', req.bodyParser().body)
     try {
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ['card'],
