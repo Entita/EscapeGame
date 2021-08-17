6b70e37243620ab186d60bbfddcb422ae9710497
Entita <entitak@gmai.com>
2021-08-17 13:40:21 +0200

    test

--- default/script.js
+++ default/script.js
@@ -57,7 +57,7 @@ var app = new Vue({
                 console.error(e.error)
             })
         },
-        validateEmail() {
+        validateEmail(email) {
             const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             return re.test(String(email).toLowerCase());
         },
