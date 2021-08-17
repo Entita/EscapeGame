1f1ba4cf1d1f3dce994312d2819284d7bf935007
Entita <entitak@gmai.com>
2021-08-17 17:46:13 +0200

    test

--- default/script.js
+++ default/script.js
@@ -133,14 +133,14 @@ var app = new Vue({
                 }).then(res => {
                     if (res.ok) return res.json()
                     return res.json().then(json => Promise.reject(json))
-                }).then(({ success }) => {
+                }).then(({ success, user }) => {
                     if (success === 'user') {
                         // User not found
                         alert('User not found')
                     } else if (success) {
                         // Logged in
                         alert('Logged in')
-                        localStorage.setItem('loginToken', email)
+                        localStorage.setItem('loginToken', user)
                         this.loginToken = localStorage.getItem('loginToken')
                     } else {
                         // Wrong password
