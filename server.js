230074dcf410117b5b18f7261c790c8e36365ebe
Entita <entitak@gmai.com>
2021-08-17 15:12:32 +0200

    test

--- server.js
+++ server.js
@@ -91,8 +91,7 @@ app.post('/login', (req, res) => {
     try {
         const email = req.body.email,
             password = req.body.password
-        console.log(users[email])
-        if (users[email] != null) {
+        if (users[email] != undefined) {
             res.json({ success: users[email].password === password })
         } else {
             res.json({ success: 'user' })
