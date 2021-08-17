5bec0d38e42672965c7f73150d5d6a966e081763
Entita <entitak@gmai.com>
2021-08-17 11:54:06 +0200

    test

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ client.sadd('users', JSON.stringify(new_user))
 client.smembers('users', (err, reply) => {
     console.log(reply)
     reply.map(user => {
-        console.log('user', user)
-        console.log(user['email'], user['username'], user['password'])
+        console.log('user', typeof user)
+        console.log(user.email, user.username, user.password)
     })
 })
 
